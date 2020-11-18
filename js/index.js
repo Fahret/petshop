d8693b04d4e901c5a27ae5e5947f4d9b2cbebab9
fahret <brunobergamini12345@gmail.com>
2020-11-18 20:47:49 -0300

    corrigindo tudo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
     $(".produto-botao").click(function(){
-=======
-    $(".botao-todos-produtos").click(function(){
->>>>>>> google-api
 
         window.location.href = "produtos.html"
 
@@ -21,22 +17,9 @@ function initMap() {
 
   var configuracoes = {
     center: {lat: -25.45212754, lng: -49.25276682},
-<<<<<<< HEAD
     zoom: 30
-=======
-    zoom: 15
->>>>>>> google-api
   }
       
   mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
 
-<<<<<<< HEAD
-=======
-  var marcador = new google.maps.Marker({
-    position: {lat: -25.45212754, lng: -49.25276682},
-    title: "PUCPR",
-    map: mapa
-  });
-
->>>>>>> google-api
 }
